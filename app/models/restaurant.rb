cdcce3ba2c5e36eccf3ad6a4031c90b1b8b071c5
Peter Hempel <hempel.pe@gmail.com>
2020-05-20 12:36:55 +0200

    models created and validated

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+# Restaurant Model with Validations
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+end
